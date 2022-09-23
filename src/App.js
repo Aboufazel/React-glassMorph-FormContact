7feb6c037554a0ceb542c4eab1ede34f7b25bf98
Aboufazel <a.ab2521@gmail.com>
2022-09-23 18:02:56 +0330

    Filter icon Edit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import AddContact from "./AddContact/AddContact";
 
 function App() {
     const [contact , setContact] = useState([
-        {id:1 , name: 'Abbas' , age:25 ,mail:'a.ab2521@gmail.com', phone:'09178272816' , country:'iran' , favorite:'yes'},
-        {id:2 , name: 'Alireza' , age:25 , mail:'t.alireza@gmail.com', phone:'09373075204' , country:'iran' , favorite:'no'},
+        {id:1 , name: 'Abbas' , age:25 ,mail:'a.ab2521@gmail.com', phone:'09178272816' , country:'iran' , favorite:'F'},
+        {id:2 , name: 'Alireza' , age:25 , mail:'t.alireza@gmail.com', phone:'09373075204' , country:'iran' , favorite:'N'},
     ])
   return (
      <div>
